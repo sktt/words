8895edc66cec66c774e40bc113cb88494e6bdcdb
Johannes <johannes.wikner@gmail.com>
2016-01-18 21:47:40 +0100

    Improve

--- main.hs
+++ main.hs
@@ -1,14 +1,14 @@
 import Data.List (unfoldr, tails)
-import System.Random (newStdGen, StdGen(..), random)
+import System.Random (randomRIO, newStdGen, StdGen(..), random)
 import Data.Text (pack, unpack, replace)
 
 ----
--- This thing randomizes a text using the input text `seed`. Like markov chains
+-- This thing randomizes a text using the input text `input`. Like markov chains
 --
 -- It uses the first 5 chars as a start `window` and then randomly selects one
--- of the possible following characters (given seed). Continues until 2000 chars
+-- of the possible following characters (given input). Continues until 2000 chars
 -- are there or the thing crashes because there are no possiible candidates for
--- next char for the given `window` causing 0 mod 0. 
+-- next char for the given `window` causing 0 mod 0.
 --
 -- ex: $ ./words < textFile
 ----
@@ -17,26 +17,30 @@ import Data.Text (pack, unpack, replace)
 nLast :: Int -> [a] -> [a]
 nLast n xs = foldl (const . tail) xs (drop n xs)
 
--- array of possible preceding characters after `window` given `seed`
+-- array of possible preceding characters after `window` given `input`
 candidates :: Eq a => [a] -> [a] -> [a]
-candidates seed window = (map head -- get the first element after last element in `window`
-  . filter (not . null) -- filter empty array
-    . map (drop wl) -- drop the the part that is already in `window`
-      . filter ((==) window . take wl) -- filter for suffixes starting with `window`
-        . tails) seed -- create suffix array of seed
+candidates input curr = (map head -- get the first preceding el for every occurence
+  . filter (not . null) -- filter empty
+  . map (drop wl) -- drop the the part that is already in `window`
+  . filter ((window ==) . take wl) -- find where `window` exists in `input`
+  . tails) input
   where wl = length window
+        window = nLast 5 curr
 
 -- recursively append next char
 generate :: Eq a => [a] -> [a] -> StdGen -> [[a]]
-generate seed start g = map snd $ unfoldr lam (g, start)
-  where lam (g', curr) = Just ((g', curr), (gNext g', curr ++ [randomElem (candidates seed (win curr)) (r g')]))
-        win        = nLast 5
-        r          = fst . (random :: StdGen -> (Int, StdGen))
-        gNext      = snd . (random :: StdGen -> (Int, StdGen))
-        randomElem arr n = arr !! mod n (length arr)
+generate input start g = map snd (unfoldr lam (g, start))
+  where lam (g, curr) = Just ((g, curr), appendNextEl curr (nextEl (g, candidates input curr)))
+
+appendNextEl :: [b] -> (a, b) -> (a, [b])
+appendNextEl a (b, c) = (b, a ++ [c])
+
+nextEl :: (StdGen, [a]) -> (StdGen, a)
+nextEl (g, candidates) = (g', candidates !! mod r (length candidates))
+  where (r, g') = random g :: (Int, StdGen)
 
 main = do
-  seed <- getContents
-  g <- newStdGen 
-  let start = take 5 seed
-  putStrLn $ last . take 5000 $ generate seed start g
+  input <- getContents
+  g <- newStdGen
+  let start = take 5 input
+  putStrLn $ last . take 2000 $ generate input start g
